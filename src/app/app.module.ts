f97afe903dbcf112d787f51a1afe9d5c4b1fa875
mamjerez <correomam@gmail.com>
2018-12-25 18:56:26 +0100

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BorrarComponent } from './borrar/borrar.component';
+import { MapaComponent } from './mapa/mapa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BorrarComponent,
+    MapaComponent
   ],
   imports: [
     BrowserModule,
